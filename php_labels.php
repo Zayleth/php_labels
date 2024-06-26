a8e89c4029d572e89fc9f6f9f1631aaf8a583dbb
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-06-26 17:00:09 -0400

    signs_practice

---
+++ php_labels.php
@@ -0,0 +1,44 @@
+<?php
+if ($_REQUEST["cod"] == 1) { ?>
+<h2>Libra</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 2) { ?>
+<h2>Geninis</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 3) { ?>
+<h2>Cancer</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 4) { ?>
+<h2>Leo</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 5) { ?>
+<h2>Tauro</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 6) { ?>
+<h2>Virgo</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 7) { ?>
+<h2>Acuario</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 8) { ?>
+<h2>Piscis</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 10) { ?>
+<h2>Capricornio</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 11) { ?>
+<h2>Escorpio</h2>
+
+<?php }
+if ($_REQUEST["cod"] == 12) { ?>
+<h2>Aries</h2>
+<?php }
